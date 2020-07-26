306ca40000637481761a335d8ff8b00a222a8109
avnyu <avnyusa@gmail.com>
2020-07-26 22:47:20 +0700

    update

---
+++ facebook-hacker-cup/2020/qualification-round/a.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define ll long long
+#define ii pair<int, int>
+#define vi vector<int>
+#define vii vector<ii>
+#define vll vector<ll>
+#define vvi vector<vector<int>>
+#define vvii vector<vector<ii>>
+#define vvll vector<vector<ll>>
+#define fi first
+#define se second
+#define pb push_back
+#define eb emplace_back
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define uni(v) v.erase(unique(v.begin(), v.end()), v.end())
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (ll) a / __gcd(a, b) * b
+
+using namespace std;
+
+void print();
+template <typename T, typename... Args>
+void print(T x, Args... args);
+
+void solve(int T) {
+    int n;
+    cin >> n;
+    string in, out;
+    cin >> in >> out;
+
+    cout << "Case #" << T << ":\n";
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < n; ++j) {
+            bool can = true;
+            if (i < j) {
+                for (int k = i; k++ < j;) can &= in[k] == 'Y';
+                for (int k = i; k < j; ++k) can &= out[k] == 'Y';
+            } else {
+                for (int k = i; k-- > j;) can &= in[k] == 'Y';
+                for (int k = i; k > j; --k) can &= out[k] == 'Y';
+            }
+            cout << "NY"[can] << (j == n - 1 ? "\n" : "");
+        }
+}
+int main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    freopen("in", "r", stdin);
+    freopen("out", "w", stdout);
+
+    int t = 1;
+    cin >> t;
+    for (int i = 0; i++ < t;) solve(i);
+
+    return 0;
+}
+
+void print() { cout << "\n"; }
+template <typename T, typename... Args>
+void print(T x, Args... args) {
+    if (sizeof...(args)) {
+        cout << x << ' ';
+        print(args...);
+    } else {
+        cout << x << '\n';
+    }
+}
