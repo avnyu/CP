27f70bbe500e79c5a401011a96c25ac07956ac4d
avnyu <avnyusa@gmail.com>
2020-08-02 16:38:21 +0700

    update

--- c++/a.cpp
+++ c++/a.cpp
@@ -27,6 +27,7 @@ template <typename T, typename... Args>
 void print(T x, Args... args);
 
 void solve() {
+    
 }
 int main() {
     ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
