653d2bee726e0eeb573eec152c5fd56da55a0392
avnyu <avnyusa@gmail.com>
2020-11-03 23:36:03 +0700

    update

--- icpc/2018-2019 ACM-ICPC, Asia Shenyang Regional Contest/g.cpp
+++ icpc/2018-2019 ACM-ICPC, Asia Shenyang Regional Contest/g.cpp
@@ -32,68 +32,70 @@ vvll dis(M);
 map<ll, ll> p;
 
 void init() {
-    for (int i = 1; i < N; ++i)
-        for (int j = 1; j < N; ++j)
+    for (ll i = 1; i < N; ++i)
+        for (ll j = 1; j < N; ++j)
             if (i * i + j * j < M) {
-                int t = i * i + j * j;
-                int x = sqrt(.5 + t);
+                ll t = i * i + j * j;
+                ll x = round(sqrt(t));
                 if (x * x == t) dis[t].push_back(i * N + j);
             }
 }
-void add(int x, int y, int w) { p[x * N + y] = w; }
-void rem(int x, int y) { p.erase(x * N + y); }
-void inc(int x, int y, int w) {
+void add(ll x, ll y, ll w) { p[x * N + y] = w; }
+void rem(ll x, ll y) {
+    if (p.find(x * N + y) != p.end()) p.erase(x * N + y);
+}
+void inc(ll x, ll y, ll w) {
     if (p.find(x * N + y) != p.end()) p[x * N + y] += w;
 }
-void inc(int x, int y, int k, int w) {
+void inc(ll x, ll y, ll k, ll w) {
     if (k == 0) {
         inc(x, y, w);
         return;
     }
-    ll l = sqrt(0.5 + k);
+    ll l = round(sqrt(k));
     if (l * l == k)
-        for (int i = -1; i < 2; i += 2) {
+        for (ll i = -1; i < 2; i += 2) {
             inc(x + i * l, y, w);
             inc(x, y + i * l, w);
         }
     for (auto& ps : dis[k])
-        for (int i = -1; i < 2; i += 2)
-            for (int j = -1; j < 2; j += 2) {
-                int x2 = x + (ps / N) * i, y2 = y + (ps % N) * j;
+        for (ll i = -1; i < 2; i += 2)
+            for (ll j = -1; j < 2; j += 2) {
+                ll x2 = x + (ps / N) * i, y2 = y + (ps % N) * j;
                 inc(x2, y2, w);
             }
 }
-ll get(int x, int y) { return p.find(x * N + y) != p.end() ? p[x * N + y] : 0; }
-ll get(int x, int y, int k) {
+ll get(ll x, ll y) { return (p.find(x * N + y) != p.end()) ? p[x * N + y] : 0; }
+ll get(ll x, ll y, ll k) {
     if (k == 0) {
         return get(x, y);
     }
     ll res = 0;
-    int l = sqrt(0.5 + k);
+    ll l = round(sqrt(k));
     if (l * l == k)
-        for (int i = -1; i < 2; i += 2) {
+        for (ll i = -1; i < 2; i += 2) {
             res += get(x + i * l, y);
             res += get(x, y + i * l);
         }
     for (auto& ps : dis[k])
-        for (int i = -1; i < 2; i += 2)
-            for (int j = -1; j < 2; j += 2) {
-                int x2 = x + (ps / N) * i, y2 = y + (ps % N) * j;
+        for (ll i = -1; i < 2; i += 2)
+            for (ll j = -1; j < 2; j += 2) {
+                ll x2 = x + (ps / N) * i, y2 = y + (ps % N) * j;
                 res += get(x2, y2);
             }
     return res;
 }
 void clear() { p.clear(); }
-void solve(int T) {
+void solve(ll T) {
     cout << "Case #" << T << ":\n";
     ll n, m, lastans = 0;
     cin >> n >> m;
-    for (int i = 0; i < n; ++i) {
-        int x, y, w;
+    for (ll i = 0; i < n; ++i) {
+        ll x, y, w;
         cin >> x >> y >> w;
         add(x, y, w);
     }
-    for (int i = 0; i < m; ++i) {
+    for (ll i = 0; i < m; ++i) {
         ll t, x, y, w, k;
         cin >> t >> x >> y;
         x = (x + lastans) % 6000 + 1;
@@ -120,9 +122,9 @@ int main() {
 
     init();
 
-    int t = 1;
+    ll t = 1;
     cin >> t;
-    for (int i = 0; i++ < t;) solve(i);
+    for (ll i = 0; i++ < t;) solve(i);
 
     return 0;
 }
