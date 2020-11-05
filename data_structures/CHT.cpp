0085799c1997c746397b3a470e43be949f5eedee
avnyu <avnyusa@gmail.com>
2020-11-05 11:35:29 +0700

    update

--- data_structures/CHT.cpp
+++ data_structures/CHT.cpp
@@ -1,31 +1,10 @@
 #include <bits/stdc++.h>
-
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
 #define ll long long
-#define ii pair<int, int>
 #define pll pair<ll, ll>
-#define vi vector<int>
-#define vii vector<ii>
-#define vll vector<ll>
-#define vvi vector<vector<int>>
-#define vvii vector<vector<ii>>
-#define vvll vector<vector<ll>>
 #define fi first
 #define se second
-#define pb push_back
-#define eb emplace_back
-#define all(v) v.begin(), v.end()
-#define rall(v) v.rbegin(), v.rend()
-#define uni(v) v.erase(unique(v.begin(), v.end()), v.end())
-#define gcd(a, b) __gcd(a, b)
-#define lcm(a, b) (ll) a / __gcd(a, b) * b
 
 using namespace std;
-
-void print();
-template <typename T, typename... Args>
-void print(T x, Args... args);
 
 namespace CHT {  // min CHT
 const ll M = INT64_MAX;
@@ -76,37 +55,3 @@ ll get(ll x) {
     return ans;
 }
 }  // namespace CHT
-void solve() {
-    int n, m;
-    cin >> n;
-    vector<pll> lines(n);
-    for (int i = 0; i < n; ++i) cin >> lines[i].fi >> lines[i].se;
-    CHT::add_lines(lines);
-
-    cin >> m;
-    for (int i = 0; i < m; ++i) {
-        ll x;
-        cin >> x;
-        print(CHT::get(x));
-    }
-}
-int main() {
-    ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
-
-    int t = 1;
-    // cin >> t;
-    while (t--) solve();
-
-    return 0;
-}
-
-void print() { cout << "\n"; }
-template <typename T, typename... Args>
-void print(T x, Args... args) {
-    if (sizeof...(args)) {
-        cout << x << ' ';
-        print(args...);
-    } else {
-        cout << x << '\n';
-    }
-}
--- data_structures/it_line.cpp
+++ data_structures/it_line.cpp
@@ -3,33 +3,18 @@
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 #define ll long long
-#define ii pair<int, int>
+#define pll pair<ll, ll>
 #define vi vector<int>
-#define vii vector<ii>
-#define vll vector<ll>
-#define vvi vector<vector<int>>
-#define vvii vector<vector<ii>>
-#define vvll vector<vector<ll>>
 #define fi first
 #define se second
-#define pb push_back
-#define eb emplace_back
-#define all(v) v.begin(), v.end()
-#define rall(v) v.rbegin(), v.rend()
-#define uni(v) v.erase(unique(v.begin(), v.end()), v.end())
-#define gcd(a, b) __gcd(a, b)
-#define lcm(a, b) (ll) a / __gcd(a, b) * b
 
 using namespace std;
-using namespace __gnu_pbds;
 
-void print();
-template <typename T, typename... Args>
-void print(T x, Args... args);
+namespace it_line {
 
 const int N = 1 << 17;
 const ll M = 1e18 + 7;
-vector<pair<ll, ll>> it(N << 1);
+vector<pll> it(N << 1);
 vi low(N << 1), high(N << 1);
 
 void init() {
@@ -43,7 +28,7 @@ void init() {
         cur += 1 << h;
     }
 }
-ll get(pair<ll, ll>& d, int p) { return d.fi * p + d.se; }
+ll get(pll& d, int p) { return d.fi * p + d.se; }
 void update(int i, pair<ll, ll> v) {
     if (get(it[i], low[i]) > get(v, low[i]) &&
         get(it[i], high[i]) > get(v, high[i]))
@@ -64,7 +49,7 @@ ll query(int p, int i = 1) {
     res = min(res, query(p, i << 1 | 1));
     return res;
 }
-void add(int b, int e, pair<ll, ll> v, int i = 1) {
+void add(int b, int e, pll v, int i = 1) {
     if (high[i] < b || low[i] > e) return;
     if (b <= low[i] && high[i] <= e) {
         update(i, v);
@@ -73,26 +58,4 @@ void add(int b, int e, pair<ll, ll> v, int i = 1) {
     add(b, e, v, i << 1);
     add(b, e, v, i << 1 | 1);
 }
-void solve() {}
-int main() {
-    ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
-
-    init();
-
-    int t = 1;
-    // cin >> t;
-    while (t--) solve();
-
-    return 0;
-}
-
-void print() { cout << "\n"; }
-template <typename T, typename... Args>
-void print(T x, Args... args) {
-    if (sizeof...(args)) {
-        cout << x << ' ';
-        print(args...);
-    } else {
-        cout << x << '\n';
-    }
-}
+}  // namespace it_line
