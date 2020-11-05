85813077d498e9f693ac1e7da8687f0e6e1644c4
avnyu <avnyusa@gmail.com>
2020-11-05 15:46:15 +0700

    update

--- codeforces/contest/1083/e.cpp
+++ codeforces/contest/1083/e.cpp
@@ -27,17 +27,37 @@ void print();
 template <typename T, typename... Args>
 void print(T x, Args... args);
 
-namespace DCHT {
+namespace CHT {
 #define tlll tuple<ll, ll, ll>
-
-}  // namespace DCHT
+const ll M = INT64_MAX;
+vector<tlll> cht;
+ll get(pll x, pll y) { return (x.se - y.se) / (y.fi - x.fi); }
+void add(pll line) {
+    if (st.size() && st.back().fi == line.fi) st.pop_back();
+    while () }
+void add_lines(vector<pll> lines) {
+    int n = lines.size();
+    sort(lines.begin(), lines.end());
+    for (int i = 0; i < n; ++i) add(lines[i]);
+}
+ll get(ll x) {
+    int p = int(pt.size()) - 1, add = 1 << 17;
+    while (add) {
+        if (p - add >= 0 && x < pt[p - add]) p -= add;
+        add >>= 1;
+    }
+    p--;
+    ll ans = x * st[p].fi + st[p].se;
+    return ans;
+}
+}  // namespace CHT
 
 void solve() {
     int n;
     cin >> n;
     vvll a(n, vll(3));
-    for (auto& i : a)
-        for (auto& j : i) cin >> j;
+    for (auto &i : a)
+        for (auto &j : i) cin >> j;
     sort(a.begin(), a.end());
 
     // for (int i = 0; i < n; ++i)
@@ -45,17 +65,17 @@ void solve() {
 
     ll res = 0;
     const int maxN = 1e9;
-    DLIT::add(1, maxN, pll{0, 0}, 1, 1, maxN);
+    CHT::add(1, maxN, pll{0, 0}, 1, 1, maxN);
 
     for (int i = 0; i < n; ++i) {
         auto &x = a[i][0], &y = a[i][1], &v = a[i][2];
 
-        ll cur = DLIT::query(y, 1, 1, maxN);
+        ll cur = CHT::query(y, 1, 1, maxN);
         // print("query", y, cur);
         res = max(res, cur + x * y - v);
 
         // print("add", -x, cur);
-        DLIT::add(1, maxN, pll{-x, cur + x * y - v}, 1, 1, maxN);
+        CHT::add(1, maxN, pll{-x, cur + x * y - v}, 1, 1, maxN);
     }
     print(res);
 }
