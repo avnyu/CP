b2efc2615c38dae0dafb16f92b0968d5b9f77581
avnyu <avnyusa@gmail.com>
2020-11-06 10:54:59 +0700

    update

--- data_structures/CHT.cpp
+++ data_structures/CHT.cpp
@@ -6,52 +6,30 @@
 
 using namespace std;
 
-namespace CHT {  // min CHT
+namespace CHT {
 const ll M = INT64_MAX;
-vector<pll> st;
+vector<pll> cht;
 vector<double> pt;
-double get(pll x, pll y) { return double(x.se - y.se) / (y.fi - x.fi); }
+double intersect(pll &x, pll &y) { return double(x.se - y.se) / (y.fi - x.fi); }
+void add(pll line) {
+    if (cht.size() && cht.back().fi == line.fi) {
+        pt.pop_back();
+        cht.pop_back();
+    }
+    while (cht.size() && intersect(cht.back(), line) <= pt.back()) {
+        pt.pop_back();
+        cht.pop_back();
+    }
+    pt.push_back(cht.empty() ? -M : intersect(cht.back(), line));
+    cht.push_back(line);
+}
 void add_lines(vector<pll> lines) {
     int n = lines.size();
-
-    sort(lines.rbegin(), lines.rend());
-
-    for (int i = 0; i < n; ++i) {
-        if (st.size() && st.back().fi == lines[i].fi) st.pop_back();
-
-        while (1) {
-            if (int(st.size()) < 2) {
-                st.push_back(lines[i]);
-                break;
-            }
-
-            int sts = st.size();
-            pll &x = st[sts - 2], &y = st[sts - 1], &z = lines[i];
-            double c1 = get(x, y), c2 = get(y, z);
-
-            if (c1 < c2) {
-                st.push_back(z);
-                break;
-            } else
-                st.pop_back();
-        }
-    }
-
-    pt.push_back(-M);
-    for (int i = 1; i < int(st.size()); ++i)
-        pt.push_back(get(st[i - 1], st[i]));
-    pt.push_back(M);
+    sort(lines.begin(), lines.end());
+    for (int i = 0; i < n; ++i) add(lines[i]);
 }
 ll get(ll x) {
-    int p = int(pt.size()) - 1, add = 1 << 17;
-    while (add) {
-        if (p - add >= 0 && x < pt[p - add]) p -= add;
-
-        add >>= 1;
-    }
-    p--;
-
-    ll ans = x * st[p].fi + st[p].se;
-    return ans;
+    auto p = upper_bound(pt.begin(), pt.end(), double(x)) - pt.begin() - 1;
+    return cht[p].fi * x + cht[p].se;
 }
 }  // namespace CHT
