8e02781c35a8b2125162bd1c911d4f72a509c2c0
avnyu <avnyusa@gmail.com>
2020-11-10 16:50:09 +0700

    update

--- icpc/2020 China Collegiate Programming Contest - Mianyang Site/g.cpp
+++ icpc/2020 China Collegiate Programming Contest - Mianyang Site/g.cpp
@@ -26,58 +26,73 @@ void print();
 template <typename T, typename... Args>
 void print(T x, Args... args);
 
-const int N = 207;
-int dp[N][N][N];
-vvi step = {{-1, 0, 0}, {0, -2, 1}, {0, -1, -1}};
+const int N = 20;
+int dp[N][N][N][N];
+vvi step = {{-1, 0, 0, 0}, {0, -2, 1, 0}, {0, -1, -1, 1}};
 
-int get(int c0, int c1, int c2, int c3) {
-    if (c0 + c1 + c2 + c3 == 0) return 1;
-
-    if (c1 == 0 && c2 == 0 && c3 == 0) return c0 % 2;
-
-    int win = 0;
-
-    if (c1 % 3 == 0 || (c1 % 3 == 1 && c2 == 0))
-        win = 1;
-    else
-        win = 0;
-
-    win = (win + c0) % 2;
-    return win;
-}
-int dfs(int i, int j, int k) {
-    if (i < 0 || j < 0 || k < 0 || i >= N || j >= N || k >= N) return 1;
-    if (dp[i][j][k]) return dp[i][j][k];
-    dp[i][j][k] = 2;
+int dfs(int i, int j, int k, int l) {
+    if (dp[i][j][k][l]) return dp[i][j][k][l];
+    if (i + j + k + l < 2) return dp[i][j][k][l] = 2;
+    dp[i][j][k][l] = 2;
     for (auto& s : step)
-        if (dfs(i + s[0], j + s[1], k + s[2]) == 2) dp[i][j][k] = 1;
+        if (i + s[0] >= 0 && j + s[1] >= 0 && k + s[2] >= 0 && l + s[3] >= 0 &&
+            dfs(i + s[0], j + s[1], k + s[2], l + s[3]) == 2)
+            dp[i][j][k][l] = 1;
 
-    return dp[i][j][k];
+    return dp[i][j][k][l];
 }
 void init() {
-    for (int i = 0; i < N; ++i)
-        for (int j = 0; j < N; ++j)
-            for (int k = 0; k < N; ++k) dp[i][j][k] = dfs(i, j, k);
+    for (int l = 0; l < N; ++l)
+        for (int i = 0; i < N; ++i)
+            for (int j = 0; j < N; ++j)
+                for (int k = 0; k < N; ++k) {
+                    dp[i][j][k][l] = dfs(i, j, k, l);
+                }
 }
 void solve(int T) {
-    init();
-
-    // int c0, c1, c2, c3;
-    // cin >> c0 >> c1 >> c2 >> c3;
+    int c0, c1, c2, c3;
+    cin >> c0 >> c1 >> c2 >> c3;
     vector<string> name = {"Rabbit", "Horse"};
     cout << "Case #" << T << ": ";
 
-    for (int i = 0; i < 3; ++i)
-        for (int j = 0; j < 3; ++j)
-            for (int k = 0; k < 3; ++k) {
-                print(i, j, k, dp[i][j][k] - 1, get(i, j, k, 0));
-            }
+    if (c1 == 0 && c2 == 0) {
+        if (c0 + c3 < 2)
+            print(name[1]);
+        else if (c0 == 0)
+            print(name[1]);
+        else if (c3 == 0)
+            print(name[c0 & 1]);
+        else
+            print(name[c0 % 2 == 0]);
+    } else if (c0 < N && c1 < N && c2 < N) {
+        print(name[dp[c0][c1][c2][0] - 1]);
+    } else if (c0 & 1) {
+        bool h = true;
+        if (c1 % 3 == 0) h = false;
+        if (c1 % 3 == 1 && c2 == 0) h = false;
+        if (c1 % 3 == 2 && c2 > 1) h = false;
+        print(name[h]);
+    } else {
+        bool h = true;
+        if (c1 % 3 == 1 && c2) h = false;
+        if (c1 % 3 == 2) h = false;
+        print(name[h]);
+    }
+
+    // for (int i = 0; i < N; ++i) {
+    //     print(i);
+    //     for (int j = 0; j < 7; ++j)
+    //         for (int k = 0; k < 7; ++k) cout << dp[i][j][k][1] << " \n"[k == 6];
+    //     cout << '\n';
+    // }
 }
 int main() {
     ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
 
+    init();
+
     int t = 1;
-    // cin >> t;
+    cin >> t;
     for (int i = 0; i++ < t;) solve(i);
 
     return 0;
