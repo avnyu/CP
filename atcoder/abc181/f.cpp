28bdfc5f05753362c66e205ef1caa43775d84104
avnyu <avnyusa@gmail.com>
2020-11-13 11:26:13 +0700

    update

--- atcoder/abc181/f.cpp
+++ atcoder/abc181/f.cpp
@@ -30,28 +30,57 @@ void print();
 template <typename T, typename... Args>
 void print(T x, Args... args);
 
+const int N = 107;
+vi par(N, -1);
+
+int root(int u) { return par[u] < 0 ? u : par[u] = root(par[u]); }
+void merge(int u, int v) {
+    u = root(u), v = root(v);
+    if (u == v) return;
+    par[u] += par[v];
+    par[v] = u;
+}
+void reset() { par.assign(N, -1); }
+double dis(ii u, ii v) {
+    return sqrt((u.fi - v.fi) * (u.fi - v.fi) + (u.se - v.se) * (u.se - v.se));
+}
+bool test(vii& a, double r) {
+    if (r > 100) return false;
+
+    int n = a.size();
+    for (int i = 0; i < n; ++i)
+        for (int j = i + 1; j < n; ++j)
+            if (dis(a[i], a[j]) < r + r) merge(i, j);
+
+    for (int i = 0; i < n; ++i) {
+        if (dis(a[i], ii{a[i].fi, 100}) < r + r) merge(i, n);
+        if (dis(a[i], ii{a[i].fi, -100}) < r + r) merge(i, n + 1);
+    }
+
+    return root(n) != root(n + 1);
+}
 void solve(int T) {
     int n;
     cin >> n;
-    map<int, vi> pts;
-    for (int i = 0; i < n; ++i) {
-        int x, y;
-        cin >> x >> y;
-        pts[x].push_back(y);
-    }
-    
-    vvi dp(n + 1, vi(2, 0));
-    dp[0][0] = dp[0][1] = 100;
+    vii a(n);
+    for (auto& i : a) cin >> i.fi >> i.se;
 
-    for (int i = 0; i < n; ++i) {
-        int len = i ? dis(a[i - 1], a[i]) : 100;
+    double r = 0, add = 100;
+    int cnt = 30;
+
+    while (cnt--) {
+        reset();
+        if (test(a, r + add)) r += add;
+        add /= 2;
     }
+
+    cout << fixed << setprecision(6) << r << '\n';
 }
 int main() {
     ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
 
     int t = 1;
-    cin >> t;
+    // cin >> t;
     for (int i = 0; i++ < t;) solve(i);
 
     return 0;
