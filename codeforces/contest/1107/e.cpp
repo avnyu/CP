6b5dcf71f60058520f01f6c04a33b552028139f7
avnyu <anvyusa@gmail.com>
2020-12-12 07:57:21 +0700

    update

--- codeforces/contest/1107/e.cpp
+++ codeforces/contest/1107/e.cpp
@@ -9,18 +9,28 @@ int main() {
 
     int n;
     cin >> n;
-    vi s(n + 2), a(n + 2);
-    for (int i = 1; i <= n; ++i) cin >> s[i];
+    
+    ll s[n + 2], a[n + 2];
+    memset(s, 0, sizeof(s));
+    memset(a, 0, sizeof(a));
+    
+    for (int i = 1; i <= n; ++i) {
+        char c;
+        cin >> c;
+        s[i] = c - '0';
+    }
     for (int i = 1; i <= n; ++i) cin >> a[i];
-    transform(s.begin(), s.end(), [](int x) { return x - '0'; });
+    
+    ll dp[n + 2][n + 2][2][n + 2];
+    memset(dp, 0, sizeof(dp));
 
-    ll dp[n + 2][n + 2][2][n + 2] = {};
+    for (auto &i: s) cout << i << " \n"[&i == (s + n + 1)];
 
     for (int l = 1; l <= n; ++l)
         for (int i = 1; i <= n; ++i) {
             int j = i + l - 1;
             int cnt = 0;
-            for (int k = i; k <= j; ++k) cnt += s[k] == '0'
+            for (int k = i; k <= j; ++k) cnt += s[k] == '0';
         }
 
     return 0;
