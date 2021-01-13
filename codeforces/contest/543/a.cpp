1fca0b95c2b418e4d8a07acef2d0e8e50588cd4b
avnyu <avnyusa@gmail.com>
2021-01-13 17:00:34 +0700

    cf_543_wip

--- codeforces/contest/543/a.cpp
+++ codeforces/contest/543/a.cpp
@@ -1,29 +1,45 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-int n, m, b, M;
-int a[501];
-int dp[501][501][501];
-
-int dfs(int p, int l, int t) {
-    if (p == 0) return l == 0 ? 1 : 0;
-    if (dp[p][l][t] != -1) return dp[p][l][t];
-
-    dp[p][l][t] = 0;
-    for (int i = 0; i <= l && a[p] * i <= t; ++i)
-        dp[p][l][t] = (dp[p][l][t] + dfs(p - 1, l - i, t - a[p] * i)) % M;
-
-    return dp[p][l][t];
-}
 int main() {
-    ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+    //ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
 
-    memset(dp, -1, sizeof(dp));
-
+    int n, m, b, M;
     cin >> n >> m >> b >> M;
-    for (int i = 1; i <= n; ++i) cin >> a[i];
 
-    cout << dfs(n, m, b) << '\n';
+    int a[502];
+    for (int i = 0; i < n; ++i) cin >> a[i];
+
+    int dp[2][1004][502];
+    memset(dp, 0, sizeof dp);
+    cout << "Sdf\n";
+
+    dp[0][0][0] = 1;
+
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j <= b; ++j)
+            for (int k = 0; k < n; ++k) dp[(i & 1) ^ 1][j][k] = 0;
+
+        for (int j = 0; j < b; ++j) {
+            int sum = 0;
+            for (int k = 0; k < n; ++k) {
+                auto &cur = dp[i & 1][j][k];
+                auto &nxt = dp[(i & 1) ^ 1][j + a[k]][k];
+
+                sum = (sum + cur) % M;
+                nxt = (nxt + sum) % M;
+            }
+        }
+    }
+
+    cout << "Sdf\n";
+
+    int res = 0;
+    for (int i = 0; i <= b; ++i)
+        for (int j = 0; j < n; ++j) res = (res + dp[m & 1][i][j]);
+
+    cout << res << '\n';
 
     return 0;
 }
