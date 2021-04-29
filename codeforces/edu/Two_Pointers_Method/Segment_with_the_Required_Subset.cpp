706e525858ba00c069836f4b75196a5213ddda5a
avnyu <bakavny@gmail.com>
2021-04-29 15:14:58 +0700

    cf_edu_two_pointers_method_i

--- codeforces/edu/Two_Pointers_Method/Segment_with_the_Required_Subset.cpp
+++ codeforces/edu/Two_Pointers_Method/Segment_with_the_Required_Subset.cpp
@@ -27,72 +27,76 @@ void print();
 template <typename T, typename... Args>
 void print(T x, Args... args);
 
-typedef pair<int, bitset<1000>> state;
+typedef pair<int, bitset<1001>> state;
 
+bool good(stack<state>& x, stack<state>& y, int k) {
+    if (x.empty() && y.empty())
+        return false;
+    else if (x.empty())
+        return y.top().second.test(0);
+    else if (y.empty())
+        return x.top().second.test(k);
+    else {
+        state tmp;
+        tmp.second = x.top().second & y.top().second;
+        return tmp.second.any();
+    }
+}
+void add(stack<state>& x, int v, int k) {
+    state tmp;
+
+    if (x.empty())
+        tmp.second.set(0);
+    else
+        tmp.second = x.top().second;
+
+    x.push(make_pair(v, tmp.second | (tmp.second << v)));
+}
+void rem(stack<state>& x, stack<state>& y, int k) {
+   if (y.empty()) {
+        while (x.size()) {
+            int v = x.top().first;
+            x.pop();
+
+            state tmp;
+            if (y.size())
+                tmp.second = y.top().second;
+            else
+                tmp.second.set(k);
+
+            y.push(make_pair(v, tmp.second | (tmp.second >> v)));
+        }
+    }
+    y.pop();
+}
 void solve() {
     int n, k;
     cin >> n >> k;
     vi a(n);
     for (auto& i : a) cin >> i;
 
-    stack<state> s, t;
+    stack<state> x, y;
+
     int res = n + 1;
-    state x;
-    x.second.set(0);
-
-    auto good = [](stack<state>&vs, stack<state>&vt, int vk) {
-        if (vs.empty() && vt.empty())
-            return false;
-        else if (vs.empty())
-            return vt.top().second.test(vk);
-        else if (vt.empty())
-            return vs.top().second.test(vk);
-        else {
-            for (int i = 0; i <= vk; ++i)
-                if (vs.top().second.test(i) & vt.top().second.test(vk - i))
-                    return true;
-            return false;
-        }
-    };
-
-    auto add = [] (stack<state>&vs, int v, state vx) {
-        if (vs.empty()) {
-            state tmp;
-            tmp.first = v;
-            tmp.second = vx.second | (vx.second << v);
-            vs.push(tmp);
-        } else {
-            state tmp;
-            tmp.first = v;
-            tmp.second = vs.top().second | (vs.top().second << v);
-            vs.push(tmp);
-        }
-    };
-
-    auto rem = [] (auto vadd, stack<state>&vs, stack<state>&vt, state vx) {
-        if (vt.empty()) {
-            while (!vs.empty()) {
-                vadd(vt, vs.top().first, vx);
-                vs.pop();
-            }
-        } else vt.pop();
-    };
-
     for (int i = 0, j = 0; i < n; ++i) {
-        while (j < n && !good(s, t, k)) {
-            add(s, a[j], x);
-            ++j;
+        while (j < n && !good(x, y, k)) {
+            add(x, a[j], k);
+            j++;
         }
-
-        if (good(s, t, k)) {
+        if (good(x, y, k)) {
             gmin(res, j - i);
-            print(i, j);
+            // print(i, j);
+            // if (x.size()) print(x.top().second.to_string());
+            // else print("x");
+            // print(x.size());
+            // if (y.size()) print(y.top().second.to_string());
+            // else print("y");
+            // print(y.size());
         }
-
-        rem(add, s, t, x);
+        rem(x, y, k);
     }
 
-    print(res <= n ? res : -1);
+    print(res == n + 1 ? -1 : res);
 }
 int main() {
     ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
