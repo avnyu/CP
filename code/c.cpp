4b9c7f72b20b7f3e6e0fdf8e4a3896d4037a6c6a
avnyu <bakavny@gmail.com>
2021-06-24 09:40:26 +0700

    update

--- code/c.cpp
+++ code/c.cpp
@@ -23,7 +23,8 @@ void print();
 template <typename T, typename... Args>
 void print(T x, Args... args);
 
-void solve() {}
+void solve() {
+}
 int main() {
     ios::sync_with_stdio(false);
     // cin.tie(nullptr), cout.tie(nullptr);
